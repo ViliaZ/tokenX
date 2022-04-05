c6af9f688b099c0e92f5b289f572b82cc3b19592
Vilia <vzeisig@gmail.com>
2022-04-05 20:05:52 +0200

    removed placeholders in inputs

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,7 +1,5 @@
 import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { AssetsService } from '../Services/assets.service';
-import { FooterComponent } from '../footer/footer.component';
-import { ImprintComponent } from '../imprint/imprint.component';
 import { CalculatorComponent } from '../calculator/calculator.component';
 
 @Component({
@@ -13,44 +11,35 @@ import { CalculatorComponent } from '../calculator/calculator.component';
 export class MainComponent implements OnInit {
 
   @Input() currentTab: string = 'graph'
-  @ViewChild('calculator') calculatorComponent: CalculatorComponent;
 
   public assetInfotext: any = '';
   public assetWebsite: any = ''
-  public requestedAssetID: any;
+
+  requestedAssetID: any;
 
   constructor(private assets: AssetsService) { }
 
   ngOnInit(): void {
   }
 
-  ngAfterViewInit(): any {
-    setTimeout(() => {
-      this.requestedAssetID = this.calculatorComponent.requestedAssetID;
-      this.getInfoTextData();
-    }, 2000);
-  }
 
-  ngOnChanges() {
-    console.count();
-    setTimeout(()=> {this.getInfoTextData()},1000);
-
+  // getting Data form EventEmitter (child comp: Calculator) 
+  getRequestedAssedID(assetFromCalculator: any) {
+    this.requestedAssetID = assetFromCalculator;
   }
 
   getInfoTextData() {
-    console.log('requested Asset ID neu: ', this.requestedAssetID);
+    console.log('requested Asset ID for Text: ', this.requestedAssetID);
 
-    try {
-      this.assets.getAssetDetails(this.requestedAssetID)
-        .subscribe((results: any) => {
-          this.assetInfotext = results.description.en;
-          this.assetWebsite = results.links.homepage[0];
-        })
-    } catch (error) {
-      console.error('error', error);
-    }
-
-
+    // try {
+    //   this.assets.getAssetDetails(this.requestedAssetID)
+    //     .subscribe((results: any) => {
+    //       this.assetInfotext = results.description.en;
+    //       this.assetWebsite = results.links.homepage[0];
+    //     })
+    // } catch (error) {
+    //   console.error(error);
+    // }
   }
 
 
